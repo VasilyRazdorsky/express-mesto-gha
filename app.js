17bf418a6cefd06ebaa46ef0a12e2ee891ce95d0
Vasiliy Razdorskiy <vrazdorsky@yandex.ru>
2022-12-04 13:44:39 +0300

    Исправить стиль записи кода

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const router = require("./routes/users");
+const express = require('express');
+const mongoose = require('mongoose');
+const router = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-
 app.use('/users', router);
 
-
-
-mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 }, () => {
-  console.log("connected to mongodb");
-  app.listen(PORT, () => {
-    console.log("connected to port 3000");
-  });
+  app.listen(PORT);
 });
